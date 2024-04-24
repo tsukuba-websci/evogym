9f761c9de8760e37ead9aed0e490ccf03574c3f5
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2024-04-24 17:49:23 +0900

    feat: add test to check CI

--- examples/gym_test_without_render.py
+++ examples/gym_test_without_render.py
@@ -1,5 +1,6 @@
 import gym
 import evogym.envs
+import time
 from evogym import sample_robot
 
 
@@ -8,13 +9,14 @@ if __name__ == '__main__':
     body, connections = sample_robot((5,5))
     env = gym.make('Walker-v0', body=body)
     env.reset()
+    start = time.time()
 
     while True:
         action = env.action_space.sample()-1
         ob, reward, done, info = env.step(action)
-        env.render()
+        now = time.time()
 
-        if done:
-            env.reset()
+        if done or now - start > 10:
+            break
 
     env.close()
